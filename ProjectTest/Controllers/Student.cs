696fabc5013f9bae53b3c25df7df7cc9e99ef2a5
Lường Tuấn Anh <luongtuananh.thptct@gmail.com>
2024-11-21 16:38:56 +0700

    first commit

---
+++ ProjectTest/Controllers/Student.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProjectTest.Models;
+
+namespace ProjectTest.Controllers
+{
+    public class StudentController : Controller
+    {
+        private List<Student> GetRegisteredStudents() =>
+            JsonConvert.DeserializeObject<List<Student>>(HttpContext.Session.GetString("RegisteredStudents") ?? "[]");
+        private void SaveRegisteredStudents(List<Student> students) =>
+            HttpContext.Session.SetString("RegisteredStudents", JsonConvert.SerializeObject(students));
+
+        [HttpGet]
+        public IActionResult Index() => View();
+
+        [HttpPost]
+        public IActionResult ShowKQ(Student student)
+        {
+            var students = GetRegisteredStudents();
+            students.Add(student);
+            SaveRegisteredStudents(students);
+            return RedirectToAction("ShowKQ", new { student.MSSV, student.HoTen, student.ChuyenNganh });
+        }
+        public IActionResult ShowKQ(string MSSV, string HoTen, string ChuyenNganh)
+        {
+            var students = GetRegisteredStudents();
+            var count = students.Count(s => s.ChuyenNganh == ChuyenNganh);
+            ViewBag.MSSV = MSSV;
+            ViewBag.HoTen = HoTen;
+            ViewBag.ChuyenNganh = ChuyenNganh;
+            ViewBag.SameMajorCount = count - 1;
+            return View();
+        }
+    }
+}
---
+++ ProjectTest/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Cấu hình Session
+        builder.Services.AddDistributedMemoryCache(); // Cần dùng MemoryCache để lưu trữ Session
+        builder.Services.AddSession(options =>
+        {
+            options.IdleTimeout = TimeSpan.FromMinutes(30); // Thời gian session hết hạn
+            options.Cookie.HttpOnly = true; // Cookie chỉ có thể được truy cập từ máy chủ
+            options.Cookie.IsEssential = true; // Cookie cần thiết cho việc lưu trữ
+        });
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        var app = builder.Build();
+
+        app.UseSession();
+        app.UseStaticFiles();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
+}
